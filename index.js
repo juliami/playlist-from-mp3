89c9cd1c1070490ad501f3f84c2ccbf9abb73e89
juliami <julia.miodynska@gmail.com>
2021-10-08 13:53:43 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,46 @@
+
+require('dotenv').config();
+var fs = require('fs');
+const path = require('path');
+var mm = require('musicmetadata');
+
+const rs =require('./templates/rs.js');
+
+const EXTENSION = '.mp3';
+
+var files = fs.readdirSync(process.env.INPUT_FOLDER);
+const musicFiles = files.filter(file => {
+    return path.extname(file).toLowerCase() === EXTENSION;
+});
+const output = [];
+const addSong = (string) => {
+	output.push(string);
+}
+
+function writeToFile (templateName) { 
+	const k = output.join("");
+	const outputFilePath = `${process.env.OUTPUT_FOLDER}/${templateName}_playlist.txt`;
+	fs.appendFile(outputFilePath, k, function (err) {
+		if (err) {
+		 console.log(err);
+		 console.log(`${outputFilePath} could not be created`);
+		} else {
+		  console.log(`${outputFilePath} created`);
+		}
+	  });
+}
+
+var itemsProcessed = 0;
+
+musicFiles.forEach((fileName, index, array) => {
+var path = `${process.env.INPUT_FOLDER}/${fileName}`;
+	mm(fs.createReadStream(path), function (err, metadata) {
+		if (err) throw err;
+		addSong(rs.template(metadata));
+		itemsProcessed++;
+		if(itemsProcessed === array.length) {
+			writeToFile(rs.templateName);
+		}
+ });
+});
+
---
+++ templates/rs.js
@@ -0,0 +1,8 @@
+const template = (metadata) => {
+	const { title, artist, album } = metadata;
+	const artistString = artist[0];
+	return `${artistString}	${title}	${album}\n`;
+}
+
+exports.templateName = 'rs';
+exports.template =  template;
