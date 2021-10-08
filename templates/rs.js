ef620ea8d8a314bd09e7fc43567bc97441db7cfa
juliami <julia.miodynska@gmail.com>
2021-10-08 14:07:59 +0200

    Add prettier

--- templates/rs.js
+++ templates/rs.js
@@ -1,8 +1,8 @@
 const template = (metadata) => {
-	const { title, artist, album } = metadata;
-	const artistString = artist[0];
-	return `${artistString}	${title}	${album}\n`;
-}
+  const { title, artist, album } = metadata;
+  const artistString = artist[0];
+  return `${artistString}	${title}	${album}\n`;
+};
 
-exports.templateName = 'rs';
-exports.template =  template;
+exports.templateName = "rs";
+exports.template = template;
