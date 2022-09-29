8467bee07636d1509c1462d8012fdde4785bbdc2
juliami <julia.miodynska@gmail.com>
2022-09-29 11:43:02 +0200

    add default export option

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-var fs = require("fs");
+const fs = require("fs");
 const path = require("path");
-var mm = require("musicmetadata");
+const mm = require("musicmetadata");
 const _ = require("lodash");
 const templates = require("./templates/index.js");
 const utils = require("./utils");
@@ -11,26 +11,23 @@ var files = fs.readdirSync(process.env.INPUT_FOLDER);
 const musicFiles = files.filter((file) => {
   return path.extname(file).toLowerCase() === EXTENSION;
 });
-const output = [];
-const addSong = (string) => {
-  output.push(string);
-};
 
 const templatesObject = templates.templates;
 
-const templateName = process.argv.slice(2)[0];
+const templateName = process.argv.slice(2)[0] || "all";
 const selectedTemplate = templatesObject[templateName];
 
 if (!selectedTemplate && templateName !== "all") {
-  // add colors
   console.log(`template name ${templateName} does not exist`);
   return false;
 }
 
 function processMusicFiles(t) {
+  const output = [];
+  const addSong = (string) => {
+    output.push(string);
+  };
   var itemsProcessed = 0;
-
-  console.log(t);
   musicFiles.forEach((fileName, index, array) => {
     var path = `${process.env.INPUT_FOLDER}/${fileName}`;
     mm(fs.createReadStream(path), function (err, metadata) {
--- utils.js
+++ utils.js
@@ -3,18 +3,31 @@ const _ = require("lodash");
 
 const utc = new Date().toJSON().slice(0, 10).replace(/-/g, "");
 
-module.exports = {
-  writeToFile: function (templateName, output) {
-    const sorted = _.sortBy(output, ["index"]);
-    const sortedList = sorted.map((item) => item.text).join("");
-    const outputFilePath = `${process.env.OUTPUT_FOLDER}/${utc}_${templateName}.txt`;
-    fs.appendFile(outputFilePath, sortedList, function (err) {
-      if (err) {
-        console.log(err);
-        console.log(`${outputFilePath} could not be created`);
-      } else {
-        console.log(`${outputFilePath} created`);
-      }
+const appendListToFile = (outputFilePath, list) => {
+  fs.appendFile(outputFilePath, list, function (err) {
+    if (err) {
+      console.log(err);
+      console.log(`${outputFilePath} could not be created`);
+    } else {
+      console.log(`${outputFilePath} created`);
+    }
+  });
+};
+
+const writeToFile = (templateName, output) => {
+  const sorted = _.sortBy(output, ["index"]);
+  const sortedList = sorted.map((item) => item.text).join("");
+  const outputFilePath = `${process.env.OUTPUT_FOLDER}/${utc}_${templateName}.txt`;
+  if (fs.existsSync(outputFilePath)) {
+    fs.truncate(outputFilePath, 0, () => {
+      console.log(`${outputFilePath} already exists. Updating...`);
+      appendListToFile(outputFilePath, sortedList);
     });
-  },
+  } else {
+    appendListToFile(outputFilePath, sortedList);
+  }
+};
+
+module.exports = {
+  writeToFile: writeToFile,
 };
